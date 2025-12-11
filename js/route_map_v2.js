309a13154d8fd2543acc12c26fb90a8955f3a7b2
sihoe <sigrid@gyger.no>
2025-12-11 13:50:47 +0100

    Update route_map_v2.js

--- js/route_map_v2.js
+++ js/route_map_v2.js
@@ -103,10 +103,10 @@
       '<p><span class="icon">↘</span> ' + t.descent +
       ': <strong>' + stats.descentM.toFixed(0) + "</strong> m</p>" +
       '<p><span class="icon">▲</span> ' + t.highest +
-      ': <strong> + stats.maxElevationM.toFixed(0) + "</strong> ' +
+      ': <strong> + stats.maxElevationM.toFixed(0) + "</strong>" +
       t.unit + "</p>" +
       '<p><span class="icon">▼</span> ' + t.lowest +
-      ': <strong> + stats.minElevationM.toFixed(0) + "</strong> ' +
+      ': <strong> + stats.minElevationM.toFixed(0) + "</strong>" +
       t.unit + "</p>" +
       '<p style="margin-top:16px;font-style:italic;">' +
       t.instruction +
